7e17bd3e925414364e86c549a57dc3bcaf8c4c0c
James Bubb <james@juniordevelopercentral.com>
2021-11-29 09:14:57 +0000

    feat: add github action

--- apps/rickandroll/src/main.ts
+++ apps/rickandroll/src/main.ts
@@ -4,11 +4,52 @@
  */
 
 import * as express from 'express';
+import { Request, Response } from 'express';
+import * as randomstring from 'randomstring';
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb://mongoadmin:r1ck%40r0ll@127.0.0.1:27018';
+const client = new MongoClient(url);
+const dbName = 'rickandroll';
+
+let collection;
+
+const connectToDatabase = async () => {
+  await client.connect();
+  console.log('Connected to Database Server');
+  const db = client.db(dbName);
+  collection = db.collection('urls');
+};
+
+connectToDatabase().then(console.log).catch(console.error);
+// .finally(() => client.close());
 
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to rickandroll!' });
+  res.send({ message: 'Welcome to the project!' });
+});
+
+app.post('/api/shorturl', async (req: Request, res: Response) => {
+  const randomString = randomstring.generate(6);
+  const newLink = {
+    id: randomString,
+    url: `http://localhost:3333/api/${randomString}`,
+  };
+
+  const insertResult = await collection.insert(newLink);
+  return res.json({ ...insertResult, result: newLink });
+});
+
+app.get('/api/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const findResult = await collection.findOne({ id });
+
+  if (findResult) {
+    return res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+
+  return res.send('Nothing here');
 });
 
 const port = process.env.port || 3333;
