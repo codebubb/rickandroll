302c20d3fa779f980625bd60760f8072fea70d12
James Bubb <james@juniordevelopercentral.com>
2021-12-07 14:58:04 +0000

    feat: pipeline pushes to server

--- apps/rickandroll/src/main.ts
+++ apps/rickandroll/src/main.ts
@@ -56,4 +56,6 @@ const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on('error', (error) => {
+  console.error(error);
+});
