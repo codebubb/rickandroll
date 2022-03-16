4396278786384ee458eb622bc67d17d66aba2c0d
James Bubb <james@juniordevelopercentral.com>
2022-03-16 13:44:21 +0000

    feat: shared stylesheet

--- apps/rickandroll-stats/src/main.ts
+++ apps/rickandroll-stats/src/main.ts
@@ -1,1 +1,8 @@
-import './app/app.element.ts';
+import { environment } from './environments/environment';
+
+window.onload = () => {
+  const id = window.location.pathname.split('/')[2];
+  fetch(`${environment.apiUrl}/${id}/stats`)
+    .then((response) => response.json())
+    .then(console.log);
+};
