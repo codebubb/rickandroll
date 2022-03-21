b0feba420aec73f76899ef2a03d48718abdc46ed
James Bubb <james@juniordevelopercentral.com>
2022-03-21 08:31:40 +0000

    feat: styling for stats

--- apps/rickandroll-stats/src/main.ts
+++ apps/rickandroll-stats/src/main.ts
@@ -1,8 +1,54 @@
 import { environment } from './environments/environment';
+import * as dayjs from 'dayjs';
+
+const createHeading = (text: string) => {
+  const h2Tag = document.createElement('h2');
+  h2Tag.textContent = text;
+
+  return h2Tag;
+};
+
+const createStatsTable = (
+  redirects: Array<{ accessed: string; location: string }>
+) => {
+  const tableElement = document.createElement('table');
+  const tableHeadingRow = document.createElement('tr');
+  tableHeadingRow.innerHTML = '<th>Accessed On</th><th>Location</th>';
+  tableElement.appendChild(tableHeadingRow);
+  redirects.forEach((redirect) => {
+    tableElement.appendChild(createStatsTableRow(redirect));
+  });
+
+  return tableElement;
+};
+
+const createStatsTableRow = (redirect: {
+  accessed: string;
+  location: string;
+}) => {
+  const trElement = document.createElement('tr');
+  trElement.innerHTML = `<td>${dayjs(redirect.accessed).format(
+    'HH:mm:ss DD/MM/YYYY'
+  )}</td><td>${redirect.location}<td>`;
+
+  return trElement;
+};
 
 window.onload = () => {
+  const straplineElement = document.querySelector('.strapline');
   const id = window.location.pathname.split('/')[2];
   fetch(`${environment.apiUrl}/${id}/stats`)
     .then((response) => response.json())
-    .then(console.log);
+    .then((result) => {
+      console.log(result);
+
+      if (result.status === 'ok') {
+        straplineElement.appendChild(
+          createHeading(`Viewing stats for URL: ${id}`)
+        );
+        straplineElement.appendChild(createStatsTable(result.data.redirects));
+      } else {
+        straplineElement.appendChild(createHeading(result.message));
+      }
+    });
 };
